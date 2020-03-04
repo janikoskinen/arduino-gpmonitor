66ff660bd4f6ef0e5e1f6237e353326c08b435be
Jani Koskinen <janikoskinen@iki.fi>
2020-03-04 13:44:00 +0200

    Interrupt for button state in pin 2

--- arduino-gpmonitor.ino
+++ arduino-gpmonitor.ino
@@ -8,16 +8,38 @@
 #define SERIAL_SPEED  115200
 #define DS_PIN        11
 
+#define INT_PIN       2
+#define INT_IGNORE    20
 
 // Temperature module
 DS18b20* ds;
 
+unsigned static long last_interrupt = 0;
 
+
+/*
+ * *********
+ * * SETUP * 
+ * *********
+ */
 void setup(void) {
+  // Serial port
   Serial.begin(SERIAL_SPEED);
+
+  // DS temperature sensor
   ds = new DS18b20(DS_PIN);
+
+  // Set interrupt on pin state change
+  pinMode(INT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(INT_PIN), pinChanged, CHANGE);
 }
 
+
+/*
+ * ********
+ * * LOOP *
+ * ********
+ */
 void loop(void) {
   delay(2000);
   float temp = ds->GetTemp();
@@ -25,3 +47,18 @@ void loop(void) {
   DBGLN(temp);
 }
 
+
+/*
+ * *************
+ * * Interrupt *
+ * *************
+ */
+void pinChanged() {
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt > INT_IGNORE) {
+    delay(INT_IGNORE/2);
+    DBG("Pin changed to: ");
+    DBGLN(digitalRead(INT_PIN));
+  }
+  last_interrupt = interrupt_time;
+}
